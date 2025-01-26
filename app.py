b2d81d233b7f422a970249a0e54bc1216835b7a5
tasneem0622 <59394879+tasneem0622@users.noreply.github.com>
2025-01-26 19:20:36 +0000

    Add files via upload
    streamlit app

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from langchain.document_loaders  import PyPDFLoader,Docx2txtLoader
+from doc_summarizer import doc_summarizer
+import PyPDF2
+
+st.title("Document Summarizer with Groq")
+
+st.write("Upload a document (PDF, DOCX, or TXT)")
+
+uploaded_file = st.file_uploader(" ", type=["pdf", "docx", "txt"])
+if uploaded_file:
+    file_name = uploaded_file.name.lower()
+    summary=doc_summarizer.doc_summarizer(uploaded_file)
+    st.subheader("Generated Summary:")
+    st.write(summary)
+    
